74c47ad99c324ec5b5648b8d6a0339c51b3d1bbc
KaporalClems <clement.tossam.dsr@gmail.com>
2025-08-19 20:42:17 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,55 @@
-const CACHE_NAME = 'kiosk-cache-v1';
+const CACHE_NAME = 'dsr-kiosk-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/05 ST PIERRE BY NIGHT DSR WEB HD.mp4',
-    'https://cdn.tailwindcss.com',
-    'https://srias.re/wp-content/uploads/2020/12/Destination-Sud-Reunion-png-1-min.png',
-    'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap',
-    'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap',
-    'https://flagcdn.com/w40/fr.png',
-    'https://flagcdn.com/w40/gb.png',
-    'https://flagcdn.com/w40/es.png',
-    'https://flagcdn.com/w40/de.png'
+  '/',
+  '/index.html',
+  '/accueil.html',
+  '/js/main.js',
+  '/manifest.json'
+  // Ajoutez toutes les autres pages et ressources ici
+  // '/manger.html',
+  // '/dormir.html',
+  // etc.
 ];
 
+// Installe le service worker et met en cache les actifs
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('Ouverture du cache et mise en cache des URLs');
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// Intercepte les requêtes et sert les actifs depuis le cache si disponibles
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-        .then((response) => {
-            // Le cache a une réponse
-            if (response) {
-                return response;
-            }
-            // Aucune réponse dans le cache, faire la requête réseau
-            return fetch(event.request);
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retourne la ressource depuis le cache si elle est trouvée
+        if (response) {
+          return response;
+        }
+        // Sinon, effectue une requête réseau
+        return fetch(event.request);
+      })
+  );
+});
+
+// Active le service worker et supprime les anciens caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Supprime les anciens caches
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
